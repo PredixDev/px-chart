07c3f42a681dc2ebdd003b0996b44473c581394d
Matt <Matt.Momont@ge.com>
2015-03-11 15:32:26 -0700

    Fixed up posting of ranges for time series. Added tests.

--- src/px-timeseries.js
+++ src/px-timeseries.js
@@ -2,229 +2,224 @@ define(['vruntime', 'widgets-module', 'line-chart'], function (vRuntime, module)
     'use strict';
 
     var TimeSeriesChart = vRuntime.widget.BaseDirective.extend({
-            replace: true,
-            scope: {
-                title: '=',
-                queries: '=',
-                showYAxisUnits: '=',
-                plotType: '=',
-                subtitle: '=?',
-                xAxisLabel: '=?',
-                yAxisLabel: '=?',
-                maxNumPoints: '=?'
-            },
-            template: '<div id="widget-{{$id}}" style="width:100%;margin: 0 auto"><div class="time-series-chart" style="width:100%;margin: 0 auto"></div></div>',
-            vLink: function (scope, element, attrs) {
-                this._super(scope, element, attrs);
+        replace: true,
+        scope: {
+            title: '=',
+            queries: '=',
+            showYAxisUnits: '=',
+            plotType: '=',
+            subtitle: '=?',
+            xAxisLabel: '=?',
+            yAxisLabel: '=?',
+            maxNumPoints: '=?'
+        },
+        template: '<div class="time-series-chart" style="margin: 0;"></div>',
+        vLink: function (scope, element, attrs) {
+            var self = this;
+            this._super(scope, element, attrs);
 
-                this.logger = vRuntime.logger.create('pxTimeseries');
+            this.logger = vRuntime.logger.create('pxTimeseries');
 
-                var chartConfig = this.buildConfig(scope);
-                scope.maxNumPoints = scope.maxNumPoints || 10;
+            scope.numPointsDisplayed = {};
+            scope.maxNumPoints = scope.maxNumPoints || 10;
 
-                var self = this;
-                scope.$watch('queries', function (newData, oldData) {
-                    self.dataChanged.call(self, scope, newData, oldData);
-                }, true);
+            var chartConfig = self.buildConfig(scope);
+            scope.chart = new Highcharts.StockChart(chartConfig);
 
-                scope.chart = new Highcharts.StockChart(chartConfig);
-                scope.numPointsDisplayed = {};
+            scope.$watch('queries', function (newData, oldData) {
+                self.dataChanged.call(self, scope, newData, oldData);
+            }, true);
 
-                scope.chart.reflow();
-            },
-            buildConfig: function (scope) {
-                var config = {
-                    chart: {
-                        type: 'spline',
-                        renderTo: scope.vElement.find('.time-series-chart').get(0),
-                        zoomType: 'x'
-                    },
-                    plotOptions: {
-                        series: {
-                            marker: {}
-                        }
-                    },
-                    rangeSelector: {
-                        selected: 0,
-                        inputEnabled: true,
-                        inputDateFormat: '%H:%M %m/%d/%Y',
-                        inputEditDateFormat: '%H:%M %m/%d/%Y',
-                        inputBoxWidth: 110
-                    },
-                    legend: {
-                        align: 'right',
-                        enabled: true
-                    },
+
+        },
+        buildConfig: function (scope) {
+            var self = this;
+            var config = {
+                chart: {
+                    type: 'spline',
+                    renderTo: scope.vElement.get(0),
+                    zoomType: 'x'
+                },
+                plotOptions: {
+                    series: {
+                        marker: {}
+                    }
+                },
+                rangeSelector: {
+                    selected: 0,
+                    inputEnabled: true,
+                    inputDateFormat: '%H:%M %m/%d/%Y',
+                    inputEditDateFormat: '%H:%M %m/%d/%Y',
+                    inputBoxWidth: 110
+                },
+                legend: {
+                    align: 'right',
+                    enabled: true
+                },
+                title: {
+                    text: scope.title
+                },
+                subtitle: {
+                    text: scope.subtitle
+                },
+                navigator: {
+                    adaptToUpdatedData: false
+                },
+                xAxis: {
+                    type: 'datetime',
+                    tickPixelInterval: 150,
                     title: {
-                        text: scope.title
+                        text: scope.xAxisLabel
                     },
-                    subtitle: {
-                        text: scope.subtitle
-                    },
-                    xAxis: {
-                        type: 'datetime',
-                        tickPixelInterval: 150,
-                        title: {
-                            text: scope.xAxisLabel
-                        },
-                        events: {
-                            afterSetExtremes: function (event) {
-                                console.log('setting extremes', event);
-
-                                // TODO this is where we want to do an emit, but we can't without real data (otherwise get in infinite loop with scope watch/setextremes)
-                                //scope.$emit('px-dashboard-event', 'after-set-extremes', event);
-
-                                // NOTE: this is called tons of times when rangeselector is used -- need to throttle or something
-
-                            //    var chart = $('.time-series-chart').highcharts();
-                            //    chart.showLoading('Loading data from server...');
-                            //    scope.$emit('px-dashboard-event', 'abc', 5);
-                            //    //scope.onXAxisChange({event: event}).then(function (data) {
-                            //    //    chart.series[0].setData(data);
-                            //    //    chart.hideLoading();
-                            //    //});
-                            }
-                        },
-                        minRange: 3600 * 1000 // one hour
-                    },
-                    yAxis: {
-                        title: {
-                            text: scope.yAxisLabel
-                        },
-                        labels: {}
-                    },
-                    series: []
-                };
-
-                if (scope.showYAxisUnits) {
-                    config.yAxis.labels.enabled = true;
-                }
-                else {
-                    config.yAxis.labels.enabled = false;
-                }
-
-                if (scope.plotType === 'points') {
-                    config.plotOptions.series.lineWidth = 0;
-                    config.plotOptions.series.marker.enabled = true;
-                }
-
-                return config;
-            },
-            addPoint: function (scope, chartSeries, seriesId, point) {
-                // usually, 1 point will scroll off the screen when a new one is added
-                var isScrolling = true;
-
-                // if there are less points displayed then the max number, then just add the points without scrolling
-                if (scope.maxNumPoints > scope.numPointsDisplayed[seriesId]) {
-                    isScrolling = false;
-                    scope.numPointsDisplayed[seriesId]++;
-                }
-
-                chartSeries.addPoint(point, true, isScrolling);
-            },
-            addSeries: function (scope, seriesId, data) {
-
-                var newseries = {
-                    id: seriesId,
-                    name: seriesId,
-                    data: data
-                };
-
-                scope.chart.addSeries(newseries);
-
-                // just start scrolling automatically
-                scope.numPointsDisplayed[seriesId] = Number.MAX_SAFE_INTEGER;
-            },
-            dataTransform: function (newData) {
-                var highchartSeries = [];
-                var self = this;
-                if (newData && newData.constructor === Array) {
-                    //series
-                    newData.forEach(function (query) {
-                        if (query.results && query.results.constructor === Array) {
-                            query.results.forEach(function (result) {
-                                //validate result format
-                                if (result.name && result.values && result.values.constructor === Array) {
-                                    highchartSeries.push({
-                                        name: result.name,
-                                        data: result.values
-                                    });
-                                }
-                                else {
-                                    self.logger.warn('Series data is missing name or values property');
-                                }
-                            });
+                    events: {
+                        afterSetExtremes: function (event) {
+                            scope.$emit('px-dashboard-event', 'after-set-extremes', event);
+                            self.showLoading(scope);
                         }
-                        else {
-                            //TODO : add error and angular.emit at the base Directive level
-                            self.logger.error('Invalid time series data format');
-                        }
-                    });
-                }
-                else {
-                    self.logger.error('Invalid time series data format');
-                }
+                    },
+                    minRange: 3600 * 1000 // one hour
+                },
+                scrollbar: {
+                    liveRedraw: false
+                },
+                yAxis: {
+                    title: {
+                        text: scope.yAxisLabel
+                    },
+                    labels: {}
+                },
+                series: []
+            };
 
-                return highchartSeries;
-            },
-            /*jshint unused:false */
-            dataChanged: function (scope, newData, oldData) {
+            if (scope.showYAxisUnits) {
+                config.yAxis.labels.enabled = true;
+            }
+            else {
+                config.yAxis.labels.enabled = false;
+            }
 
-                if (!newData) {
-                    //First time, angular gives us a empty string
-                    return;
-                }
+            if (scope.plotType === 'points') {
+                config.plotOptions.series.lineWidth = 0;
+                config.plotOptions.series.marker.enabled = true;
+            }
 
-                var newSeries = this.dataTransform(newData);
+            return config;
+        },
+        addPoint: function (scope, chartSeries, seriesId, point) {
+            // usually, 1 point will scroll off the screen when a new one is added
+            var isScrolling = true;
 
-                if (newSeries.length !== 0) {
+            // if there are less points displayed then the max number, then just add the points without scrolling
+            if (scope.maxNumPoints > scope.numPointsDisplayed[seriesId]) {
+                isScrolling = false;
+                scope.numPointsDisplayed[seriesId]++;
+            }
 
-                    var seriesId, chartSeries;
+            chartSeries.addPoint(point, true, isScrolling);
+        },
+        addSeries: function (scope, seriesId, data) {
 
-                    for (var index in newSeries) {
+            var newseries = {
+                id: seriesId,
+                name: seriesId,
+                data: data
+            };
 
-                        if (newSeries[index] && newSeries[index].name && newSeries[index].data) {
+            scope.chart.addSeries(newseries);
 
-                            seriesId = newSeries[index].name;
-
-                            if (seriesId !== null) {
-                                chartSeries = scope.chart.get(seriesId);
-                                if (chartSeries) {
-                                    //Add Point to the series
-                                    for (var i = 0; i < newSeries[index].data.length; i++) {
-                                        this.addPoint(scope, chartSeries, seriesId, newSeries[index].data[i]);
-                                    }
-                                }
-                                else {
-                                    this.addSeries(scope, seriesId, newSeries[index].data);
-                                }
+            // just start scrolling automatically
+            scope.numPointsDisplayed[seriesId] = Number.MAX_SAFE_INTEGER;
+        },
+        dataTransform: function (newData) {
+            var highchartSeries = [];
+            var self = this;
+            if (newData && newData.constructor === Array) {
+                //series
+                newData.forEach(function (query) {
+                    if (query.results && query.results.constructor === Array) {
+                        query.results.forEach(function (result) {
+                            //validate result format
+                            if (result.name && result.values && result.values.constructor === Array) {
+                                highchartSeries.push({
+                                    name: result.name,
+                                    data: result.values
+                                });
                             }
                             else {
-                                this.logger.warn('seriesId is null');
+                                self.logger.warn('Series data is missing name or values property');
+                            }
+                        });
+                    }
+                    else {
+                        //TODO : add error and angular.emit at the base Directive level
+                        self.logger.error('Invalid time series data format');
+                    }
+                });
+            }
+            else {
+                self.logger.error('Invalid time series data format');
+            }
+
+            return highchartSeries;
+        },
+        /*jshint unused:false */
+        dataChanged: function (scope, newData, oldData) {
+            scope.chart.hideLoading();
+
+            if (!newData) {
+                //First time, angular gives us a empty string
+                this.showLoading(scope);
+                return;
+            }
+
+            var newSeries = this.dataTransform(newData);
+
+            if (newSeries.length !== 0) {
+
+                var seriesId, chartSeries;
+
+                for (var index in newSeries) {
+
+                    if (newSeries[index] && newSeries[index].name && newSeries[index].data) {
+
+                        seriesId = newSeries[index].name;
+
+                        if (seriesId !== null) {
+                            chartSeries = scope.chart.get(seriesId);
+                            if (chartSeries) {
+                                scope.chart.series[index].setData(newSeries[index].data);
+                            }
+                            else {
+                                this.addSeries(scope, seriesId, newSeries[index].data);
                             }
                         }
                         else {
-                            this.logger.warn('Series data is missing name or data property');
+                            this.logger.warn('seriesId is null');
                         }
                     }
-
-                    scope.chart.reflow();
+                    else {
+                        this.logger.warn('Series data is missing name or data property');
+                    }
                 }
 
-            },
-            vDestroy: function (scope) {
-                this._super(scope);
-                if (scope.chart) {
-                    scope.chart.destroy();
-                }
-                scope.vElement.remove();
             }
-        });
+            scope.chart.reflow();
+        },
+        showLoading: function (scope) {
+            scope.chart.showLoading('Loading data from server..');
+        },
+        vDestroy: function (scope) {
+            this._super(scope);
+            if (scope.chart) {
+                scope.chart.destroy();
+            }
+            scope.vElement.remove();
+        }
+    });
 
-    module.directive('pxTimeseries', function () {
-        return new TimeSeriesChart();
+    module.directive('pxTimeseries', function ($timeout) {
+        return new TimeSeriesChart($timeout);
     });
 
     return TimeSeriesChart;
-})
-;
+});
