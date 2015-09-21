96f67491b71998813cc02077749b15636630f874
Pickett, Grant Randolph <grant.pickett@ge.com>
2015-09-21 14:04:11 -0700

    Toggle series fix
    Changes made to sync toggled state across all series on a chart.
    Previously a newly added series would not be initialized properly when
    the chart had points toggled

--- px-chart.js
+++ px-chart.js
@@ -555,7 +555,7 @@ Polymer({
     if (!this.legend) {
       this.legend = this.legendRight ? this.defaultLegendRight : this.defaultLegendTop;
     }
-
+    this.pointMarkersToggled=false;
     this._initializeMargins();
 
     this.chart = new Highcharts.StockChart(this.buildConfig());
@@ -673,6 +673,9 @@ Polymer({
       if (!noRedraw) {
         this.chart.redraw();
       }
+    }
+    if(this.pointMarkersToggled){
+      this.togglePointMarkers([seriesConfig.id]);
     }
   },
 
@@ -826,6 +829,9 @@ Polymer({
       series.update({marker: {enabled: (!existingMarkerOpts || !existingMarkerOpts.enabled)}}, /*redraw*/false);
     });
     this.chart.redraw();
+    if(seriesIds == null) {
+      this.pointMarkersToggled = !this.pointMarkersToggled;
+    }
   },
 
   /**
